2901ab1df1eeedb55ca237fbb66caf8b32b1f0ff
viceAdmiralGarp <vyacheslavtihonov636@gmail.com>
2024-08-20 20:08:57 +0300

    init project

---
+++ src/main/java/com/mmdev/faceittesttask/FaceItTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mmdev.faceittesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceItTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceItTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmdev/faceittesttask/FaceItTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmdev.faceittesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceItTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
